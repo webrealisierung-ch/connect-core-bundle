7b74241fd05b2db89622c75aa985bdc57210f95e
Daniel Steuri <daniel.steuri@webrealisierung.ch>
2017-08-26 23:13:06 +0200

    Update entity
    Add OneToMany Relation to todos
    Add general getters and setters

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -2,7 +2,9 @@
 
 namespace Wr\Connect\CoreBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use \Doctrine\ORM\Mapping as ORM;
+
 
 /**
  * @ORM\Entity
@@ -22,6 +24,15 @@ class Project
      * @ORM\Column(type="integer")
      */
     private $tstamp;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Todo", mappedBy="project")
+     */
+    private $todos;
+
+    public function __construct() {
+        $this->todos = new ArrayCollection();
+    }
 
     /**
      * @ORM\Column(type="string")
@@ -61,7 +72,7 @@ class Project
     /**
      * @ORM\Column(type="integer")
      */
-    private $active;
+    private $periodOfTime;
 
     /**
      * @ORM\Column(type="integer")
@@ -72,4 +83,225 @@ class Project
      * @ORM\Column(type="integer")
      */
     private $stop;
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $closed;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getTstamp()
+    {
+        return $this->tstamp;
+    }
+
+    /**
+     * @param mixed $tstamp
+     */
+    public function setTstamp($tstamp)
+    {
+        $this->tstamp = $tstamp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTodo()
+    {
+        return $this->todo;
+    }
+
+    /**
+     * @param mixed $todo
+     */
+    public function setTodo($todo)
+    {
+        $this->todo = $todo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAlias()
+    {
+        return $this->alias;
+    }
+
+    /**
+     * @param mixed $alias
+     */
+    public function setAlias($alias)
+    {
+        $this->alias = $alias;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+    /**
+     * @param mixed $owner
+     */
+    public function setOwner($owner)
+    {
+        $this->owner = $owner;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWorkers()
+    {
+        return $this->workers;
+    }
+
+    /**
+     * @param mixed $workers
+     */
+    public function setWorkers($workers)
+    {
+        $this->workers = $workers;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProjectDir()
+    {
+        return $this->projectDir;
+    }
+
+    /**
+     * @param mixed $projectDir
+     */
+    public function setProjectDir($projectDir)
+    {
+        $this->projectDir = $projectDir;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPeriodOfTime()
+    {
+        return $this->periodOfTime;
+    }
+
+    /**
+     * @param mixed $periodOfTime
+     */
+    public function setPeriodOfTime($periodOfTime)
+    {
+        $this->periodOfTime = $periodOfTime;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStart()
+    {
+        return $this->start;
+    }
+
+    /**
+     * @param mixed $start
+     */
+    public function setStart($start)
+    {
+        $this->start = $start;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStop()
+    {
+        return $this->stop;
+    }
+
+    /**
+     * @param mixed $stop
+     */
+    public function setStop($stop)
+    {
+        $this->stop = $stop;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getClosed()
+    {
+        return $this->closed;
+    }
+
+    /**
+     * @param mixed $closed
+     */
+    public function setClosed($closed)
+    {
+        $this->closed = $closed;
+    }
 }
