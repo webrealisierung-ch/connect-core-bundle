1b57e2ae24bc90016f0efb0e029ea78d71dcf05a
Daniel Steuri <daniel.steuri@webrealisierung.ch>
2017-09-07 22:52:25 +0200

    Change namespace

--- src/Service/Todo/TodoManager.php
+++ src/Service/Todo/TodoManager.php
@@ -5,10 +5,9 @@ namespace Wr\Connect\CoreBundle\Service\Todo;
 use Contao\Backend;
 use Contao\User;
 use Doctrine\ORM\EntityManager;
-use Symfony\Bridge\Twig\TwigEngine;
+use Twig\Environment;
 use Twig\TwigFunction;
 use Wr\Connect\CoreBundle\Entity\Status;
-use Wr\Connect\CoreBundle\Entity\Todo;
 
 /**
  *
@@ -19,13 +18,13 @@ class TodoManager
     private $entityManager;
     private $twig;
 
-    public function __construct(EntityManager $entityManager, \Twig_Environment $twig)
+    public function __construct(EntityManager $entityManager, Environment $twig)
     {
         $this->entityManager = $entityManager;
         $this->twig = $twig;
 
         $function=new TwigFunction('addToBackendUrl',function($id){
-            return \Backend::addToUrl("do=projects&table=tl_wr_todo&id=".$id."&act=edit");
+            return Backend::addToUrl("do=projects&table=tl_wr_todo&id=".$id."&act=edit");
         });
 
         $this->twig->addFunction($function);
