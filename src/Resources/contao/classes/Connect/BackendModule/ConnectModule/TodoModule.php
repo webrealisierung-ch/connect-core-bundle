a8055df92c12b6d67e7e5824a62c3085fa55a1ae
Daniel Steuri <daniel.steuri@webrealisierung.ch>
2017-09-16 20:16:46 +0200

    Add Ajax handling

--- src/Resources/contao/classes/Connect/BackendModule/ConnectModule/TodoModule.php
+++ src/Resources/contao/classes/Connect/BackendModule/ConnectModule/TodoModule.php
@@ -1,6 +1,9 @@
 <?php
 namespace Wr\Connect\CoreBundle\Contao\BackendModule\ConnectModule;
 
+use Contao\Input;
+use Contao\System;
+
 class TodoModule
 {
     protected $statusTemplate = "be_mod_connect_todos";
@@ -14,8 +17,22 @@ class TodoModule
     public function compile()
     {
         $statusBoardsTemplate = new \BackendTemplate($this->statusTemplate);
+        $container=System::getContainer();
+        $statusBoards=$container->get('wr.connect.status_manager');
 
-        $statusBoards=\Contao\System::getContainer()->get('wr.connect.status_manager');
+
+        /*
+         *Preparation for Ajax Stuff
+         *
+         */
+        $statusId = Input::get('status_id');
+        $todoId = Input::get('todo_id');
+
+
+        if(!empty($todoId)&&!empty($statusId))
+        {
+            $container->get('wr.connect.todo_change_status')->changeStatus($todoId,$statusId);
+        }
 
         $statusBoardsTemplate->class='todos';
 
