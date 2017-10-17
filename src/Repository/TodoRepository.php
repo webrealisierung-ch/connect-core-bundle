960673c2be27c9072dec3c941a5569df9e08654f
Daniel Steuri <daniel.steuri@webrealisierung.ch>
2017-10-17 23:39:39 +0200

    Remove and add methods

--- src/Repository/TodoRepository.php
+++ src/Repository/TodoRepository.php
@@ -8,52 +8,11 @@ use Doctrine\ORM\EntityRepository;
 class TodoRepository extends EntityRepository
 {
 
-    /**
-     * @return Todo[]
-     */
-   public function findOpenByUserOrderedByDate(BackendUser $User,$order="ASC"){
-       return $this->createQueryBuilder('todo')
-           ->where('todo.author= :user')
-           ->setParameter(':user', $User->id)
-           ->andWhere('todo.status= :isActive')
-           ->setParameter('isActive', 0)
-           ->orderBy('todo.tstamp',$order)
-           ->getQuery()
-           ->execute();
-   }
 
     /**
      * @return Todo[]
      */
-    public function findProgressByUserOrderedByDate($user,$order="ASC"){
-        return $this->createQueryBuilder('todo')
-            ->where('todo.author= :user')
-            ->setParameter(':user', $user->id)
-            ->andWhere('todo.status= :isActive')
-            ->setParameter('isActive', 1)
-            ->orderBy('todo.tstamp',$order)
-            ->getQuery()
-            ->execute();
-    }
-
-    /**
-     * @return Todo[]
-     */
-    public function findClosedByUserOrderedByDate($user,$order="ASC"){
-        return $this->createQueryBuilder('todo')
-            ->where('todo.author= :user')
-            ->setParameter(':user', $user->id)
-            ->andWhere('todo.status= :isActive')
-            ->setParameter('isActive', 2)
-            ->orderBy('todo.tstamp',$order)
-            ->getQuery()
-            ->execute();
-    }
-
-    /**
-     * @return Todo[]
-     */
-    public function findStatusByUserOrderedByDate($user,$statusId,$order="ASC"){
+    public function findByStatusByUserOrderedByDate($user,$statusId,$order="ASC"){
         return $this->createQueryBuilder('todo')
             ->where('todo.author= :user')
             ->setParameter(':user', $user->id)
@@ -76,6 +35,22 @@ class TodoRepository extends EntityRepository
             ->innerJoin('todo.project','project')
             ->andWhere("project.closed= :isClosed")
             ->setParameter('isClosed',0)
+            ->orderBy('todo.id',$order)
+            ->getQuery()
+            ->execute();
+    }
+
+    /**
+     * @return Todo[]
+     */
+    public function findByStatusAndByUserAndByShowAfterDays($user,$statusId,$time,$order="DESC"){
+        return $this->createQueryBuilder('todo')
+            ->where('todo.author= :user')
+            ->setParameter(':user', $user->id)
+            ->andWhere('todo.status= :statusId')
+            ->setParameter('statusId', $statusId)
+            ->andWhere('timestamp > :time')
+            ->setParameter('time',$time)
             ->orderBy('todo.id',$order)
             ->getQuery()
             ->execute();
